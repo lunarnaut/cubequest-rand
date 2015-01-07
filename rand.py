c41041cec6dbdb1fcfafac6a5737081c2354f974
Michael Pratt <michael@pratt.im>
2015-01-07 11:31:16 -0500

    Initial commit
    Generate a seed from team key and current time.
    
    I am not certain that the endianness on the 32-bit segments is what the
    rules are looking for.

---
+++ rand.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+
+import argparse
+import datetime
+import random
+import string
+
+a = 1664525
+b = 1013904223
+M = 2**32
+
+MASK32 = 2**32 - 1
+
+def random_key():
+    key = ''
+
+    for _ in range(256):
+        key += random.choice(string.ascii_letters + string.digits)
+
+    return key
+
+def generate_seed(key, timestamp):
+    """
+    XOR each 32-bit component together.
+    key is 256-bit ascii, timestamp 128-bit ascii
+    """
+    ret = 0
+
+    key = key.encode('ascii')
+    key = int.from_bytes(key, byteorder='big')
+
+    ret ^= key & MASK32
+    ret ^= (key >> 32) & MASK32
+    ret ^= (key >> 64) & MASK32
+    ret ^= (key >> 96) & MASK32
+    ret ^= (key >> 128) & MASK32
+    ret ^= (key >> 160) & MASK32
+    ret ^= (key >> 224) & MASK32
+
+    timestamp = timestamp.encode('ascii')
+    timestamp = int.from_bytes(timestamp, byteorder='big')
+
+    ret ^= timestamp & MASK32
+    ret ^= (timestamp >> 32) & MASK32
+    ret ^= (timestamp >> 64) & MASK32
+    ret ^= (timestamp >> 96) & MASK32
+
+    return ret
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Generate CubeQuest random data')
+    parser.add_argument('--team-key', type=str, help='Team key',
+                        default=random_key())
+    args = parser.parse_args()
+
+    print('Team key: %s' % args.team_key)
+
+    now = datetime.datetime.now(datetime.timezone.utc)
+    now = now.strftime('%Y%m%d%H%M%S.%f')
+
+    # Silly hack to reduction precision to 1/10 sec
+    now = now[:-5]
+
+    print('Timestamp: %s' % now)
+
+    seed = generate_seed(args.team_key, now)
+
+    print('Seed: %d' % seed)
